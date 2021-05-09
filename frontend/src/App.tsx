e1f66805a28aa21f0d9bab51588072aa86672431
Douglas Nishiama <dnishiama@gmail.com>
2021-05-09 12:03:12 -0300

    First request

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,3 @@
-import Footer from "components/Footer"
-import NavBar from "components/NavBar"
-import DataTable from "components/DataTable"
-import BarChart from "components/BarChart"
-import DonutChart from "components/DonutChart"
-import React from "react";
 import Routes from "Routes"
 
 function App() {
